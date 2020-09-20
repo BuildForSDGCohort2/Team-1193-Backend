b9a046c148d7c946b49c9f9af280db1ce8ead9c7
juma-vincent <vinsoncode@gmail.com>
2020-09-20 19:56:55 +0300

    added register controller

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const cors = require("cors");
 const knex = require("knex");
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-<<<<<<< HEAD
-const farmproduce = require("./controllers/farmproduce");
-=======
 const enforce = require("express-sslify");
->>>>>>> 6b5f6ee70bc453af0476c236227ad5866a9eb7a9
+const farmproduce = require("./controllers/farmproduce");
+const register = require("./controllers/register");
 
 const db = knex({
   client: "pg",
@@ -35,25 +33,7 @@ app.get("/farmproduce", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({ name: name, email: loginEmail[0], joined: new Date() })
-          .then((user) => res.json(user[0]));
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch((error) => res.status(400).json("unable to register"));
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 app.post("/signin", (req, res) => {
@@ -92,13 +72,6 @@ app.post("/payment", (req, res) => {
       res.status(200).send({ success: stripeRes });
     }
   });
-<<<<<<< HEAD
-});
-
-app.get("/payment", (req, res) => {
-  res.send("payment is working");
-=======
->>>>>>> 6b5f6ee70bc453af0476c236227ad5866a9eb7a9
 });
 
 app.listen(process.env.PORT || 5000, () => {
